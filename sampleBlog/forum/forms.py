db8df70b9b29917bec9848b7145b84ae0aa15de4
ZheZheDoshka <grayfox380@gmail.com>
2023-07-08 21:17:45 +0300

    forum model and forms work

--- sampleBlog/forum/forms.py
+++ sampleBlog/forum/forms.py
@@ -5,11 +5,15 @@ from forum.models import Category, SubCategory, Topic, Post
 
 
 class CategoryForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Category
+        fields = ('category_name', 'category_description')
 
 
 class SubCategoryForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = SubCategory
+        fields = ('subcategory_name', 'subcategory_description')
 
 
 class TopicForm(forms.ModelForm):
--- sampleBlog/forum/models.py
+++ sampleBlog/forum/models.py
@@ -5,26 +5,41 @@ from core.models import User
 
 
 class Category(models.Model):
+    slug = models.SlugField(unique=True)
     category_name = models.CharField(max_length=255,)
+    category_description = models.CharField(max_length=255,)
 
 
 class SubCategory(models.Model):
+    slug = models.SlugField(unique=True)
     subcategory_name = models.CharField(max_length=255,)
+    subcategory_description = models.CharField(max_length=255, )
     category = models.ForeignKey(Category,
                                  on_delete=models.CASCADE, null=True)
 
 
 class Topic(models.Model):
-    topic_name = models.CharField(max_length=255,)
+    title = models.CharField(max_length=255,)
     subcategory = models.ForeignKey(SubCategory,
                                     on_delete=models.CASCADE, null=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    last_update_date = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE, null=True)
 
+    def get_post_number(self):
+        pass
+
+    def get_page(self):
+        pass
+
+    def get_url(self):
+        pass
+
 
 class Post(models.Model):
-    text = models.CharField(max_length=255,)
-    user = models.ForeignKey(Topic,
-                             on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=5000,)
+    topic = models.ForeignKey(Topic,
+                              on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User,
                              on_delete=models.CASCADE, null=True)
