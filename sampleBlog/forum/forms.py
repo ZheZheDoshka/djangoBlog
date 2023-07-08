7833542c84fcdcebfbbc53666d0ee740a8c0d0a2
ZheZheDoshka <grayfox380@gmail.com>
2023-07-08 20:33:36 +0300

    making placeholder forms for forum models

--- sampleBlog/forum/forms.py
+++ sampleBlog/forum/forms.py
@@ -3,3 +3,18 @@ from django import forms
 
 from forum.models import Category, SubCategory, Topic, Post
 
+
+class CategoryForm(forms.ModelForm):
+    pass
+
+
+class SubCategoryForm(forms.ModelForm):
+    pass
+
+
+class TopicForm(forms.ModelForm):
+    pass
+
+
+class PostForm(forms.ModelForm):
+    pass
