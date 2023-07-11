11658f4b9c2243debc6c69e7250d1a95c642b4ae
ZheZheDoshka <grayfox380@gmail.com>
2023-07-11 18:23:25 +0300

    made admin page more readable

--- sampleBlog/forum/admin.py
+++ sampleBlog/forum/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from forum.models import *
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["category_name", "category_description", "category_position"]
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ["subcategory_name", "subcategory_description", "subcategory_position"]
+
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(SubCategory)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
